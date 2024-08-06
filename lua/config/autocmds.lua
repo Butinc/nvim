12f4ab9b95126b80c5f0c84712a548ee1ed26eaa
butinc <1341219953@qq.com>
2024-08-06 15:05:31 +0800

    Update autocmds.lua, add wsl clipboard

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,14 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+-- System clipboard
+local in_wsl = os.getenv("WSL_DISTRO_NAME") ~= nil
+
+if in_wsl then
+  vim.g.clipboard = {
+    name = "wsl clipboard",
+    copy = { ["+"] = { "clip.exe" }, ["*"] = { "clip.exe" } },
+    paste = { ["+"] = { "nvim_paste" }, ["*"] = { "nvim_paste" } },
+    cache_enabled = true,
+  }
+end
